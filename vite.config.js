be662030774f3bbfb062b808a566b064808ee2ee
nghia <2002nghiantt@gmail.com>
2025-08-22 09:21:36 +0700

    1111111

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5175,
+    port: 5174,
     open: true, // Automatically open the browser
   },
 })
